00a3d4e88c8b6d2a4e3fb4856ff485271cccc926
vadymvoitko <vadym.voitko@gmail.com>
2018-12-11 20:51:25 +0200

    lesson 1 done

--- src/components/GameCanvas.vue
+++ src/components/GameCanvas.vue
@@ -2,7 +2,12 @@
   <div class="alert alert-secondary greeting">
     <h1 class="pb-20">Please solve the below equation</h1>
     <h2> {{ x }} + {{ y }} = {{ result }} </h2>
-
+    <button
+      v-for="(item, index) in answerOptions"
+      :key="index"
+    >
+    {{ item }}
+    </button>
   </div>
 </template>
 
@@ -13,8 +18,31 @@ export default {
     return {
       x: 0,
       y: 0,
-      result: '?'
+      result: '?',
+      from: -100,
+      to: 100,
+      expectedResult: 0,
+      numberOfButtons: 3,
+      answerOptions: []
     }
+  },
+  methods: {
+    generateNumber(from, to) {
+      return Math.ceil(Math.random()*(to - from) + from)
+    },
+    generateMembers(){
+      this.x = this.generateNumber(this.from, this.to);
+      this.y = this.generateNumber(this.from, this.to);
+      this.expectedResult = this.x + this.y
+      this.answerOptions.push(this.expectedResult)
+      for (let i=0; i < this.numberOfButtons - 1; i++) {
+        const newOption = this.expectedResult + this.generateNumber(-10, 10);
+        this.answerOptions.push(newOption)
+      }
+    }
+  },
+  created () {
+    this.generateMembers()
   }
 }
 </script>
